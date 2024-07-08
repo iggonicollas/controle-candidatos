81077934fc195ff9238328aad30f3d46b7eb620d
iggonicollas <iggonicollas@gmail.com>
2024-07-08 12:02:24 -0300

    insercao de array

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,12 +1,36 @@
 package candidatura;
-
+import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
-
+  
     public static void main(String[] args) {
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        selecaoCandidatos();
+        
     }
+    static void selecaoCandidatos() {
+        String [] candidatos = {"Lucas", "Mariana", "Pedro", "Ana", "João", "Clara", "Carlos", "Sofia", "Bruno", "Laura", "Miguel", "Julia", "Rafael", "Beatriz", "Gabriel"};
+
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionados < 5){
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+            
+            System.out.println("O candidato " + candidato + " Solicitou este valor de salario " + salarioPretendido);
+                if(salarioBase >= salarioPretendido) {
+                    System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                    candidatosSelecionados++;
+                }
+                candidatosAtual++;
+        }
+
+    }
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+
+    }
+
+
 
     static void analisarCandidato(double salarioPretendido) {
         double salarioBase = 2000.0;
